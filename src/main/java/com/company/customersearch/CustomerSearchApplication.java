05f8863cd9acd4156e3b3b132417e770cd883f91
HariOne <gangetihari@gmail.com>
2026-05-10 13:05:32 +0530

    Add main application class

---
+++ src/main/java/com/company/customersearch/CustomerSearchApplication.java
@@ -0,0 +1,12 @@
+package com.company.customersearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerSearchApplication.class, args);
+    }
+}
