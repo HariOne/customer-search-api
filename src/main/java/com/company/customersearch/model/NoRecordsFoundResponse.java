687783d215d45e74d4382709b19dc0d940b109b7
HariOne <gangetihari@gmail.com>
2026-05-10 13:20:47 +0530

    Add model classes: Customer, Address, LoyaltyDetails, and ErrorResponse

---
+++ src/main/java/com/company/customersearch/model/NoRecordsFoundResponse.java
@@ -0,0 +1,17 @@
+package com.company.customersearch.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class NoRecordsFoundResponse {
+
+    @JsonProperty("message")
+    private String message;
+}
